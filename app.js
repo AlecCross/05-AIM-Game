7fe1effb89f26301cfabdc3ac5a1d254e54f5905
AlecCross <cross2472@gmail.com>
2021-06-21 15:02:24 +0300

    Установка времени
    При нажатии на одну из кнопок 10 20 30сек устанавливается время в переменную time

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const startBtn = document.querySelector('#start')
 //Для работы с экранами собираем их все вместе
 const screens = document.querySelectorAll('.screen')
+//Список выбора времени 10 20 30
+const timeBtn = document.querySelector('#time-list')
+let time = 0
 
 startBtn.addEventListener('click', (event) => {
     //Если нажать на "Начать игру"
@@ -13,4 +16,20 @@ startBtn.addEventListener('click', (event) => {
     event.preventDefault()
     //Чтобы показать следующий экран
     screens[0].classList.add('up')
+})
+
+timeBtn.addEventListener('click', (event) => {
+    //слушатель на весь блок списка, но нам нужно слушать каждый эл отдельно
+    //Делегирование событий
+    if(event.target.classList.contains('time-btn')) {
+        
+        console.log(event.target.getAttribute('data-time'))
+        //Теперь нужно определить какую кнопку нажал и сколько времени нужно передать в игру
+        //Создадим time = 0 и в html добавим атрибуты к 10 20 30 с соответственными значениями
+        //data-time="10"...
+
+        time = parseInt(event.target.getAttribute('data-time'))
+    }
+    
+
 })
